5d12c1d902da8dc957ebdee023772d7a16809b25
YUN-SEOK LEE <seok000908@gmail.com>
2025-04-19 01:01:52 +0900

    기본 구조 세팅

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-// export default class App{
-//   constructor() {
-//     this.el = document.createElement('div');
-//     this.el.textContent = 'Hello, world!';
-//   }
-// }
-
 import { Component } from './core/core';
+import TheHeader from './components/TheHeader';
 
 export default class App extends Component {
-  constructor(){
-    super({
-      state: {
-        inputText: '',
-      }
-    })
-  }
   render() {
-    this.el.classList.add('search');
-    this.el.innerHTML = /* html */`
-      <input />
-      <button>Click!</button>
-    `;
+    const routerView = document.createElement('router-view');
 
-    const inputEl = this.el.querySelector('input');
-    inputEl.addEventListener('input', () => {
-      this.state.inputText = inputEl.value;
-    });
-    
-    const buttonEl = this.el.querySelector('button');
-    buttonEl.addEventListener('click', () => {
-      console.log(this.state.inputText); 
-    });
+    this.el.append(
+      new TheHeader().el,
+      routerView
+    );
   }
 }
--- src/core/core.js
+++ src/core/core.js
@@ -3,13 +3,29 @@ export class Component{
   constructor(payload = {}) {
     const { 
       tagName = 'div',
-      state = {}
+      state = {},
+      props = {}
     } = payload;
     this.el = document.createElement(tagName);
     this.state = state;
+    this.props = props;
     this.render(); 
   }
   render() {
     // ...
+  }
+}
+
+
+
+
+//// Router ////
+
+export function createRouter(routes) {
+  return function() {
+    window.addEventListener('popstate', () => {
+      routeRender(routes);
+    });
+    routeRender(routes);
   }
 }
